187967fad31018a31d53dda2c78880b3fd0efbe7
Abdullah0505 <>
2021-05-03 20:25:11 +0000

    Linter Error fix

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.getElementById('change-image').addEventListener('click', image)
 document.getElementById('change-text').addEventListener('click', text)
 document.getElementById('hide').addEventListener('click', hide)
-document.getElementById('show').addEventListener('click',show)
+document.getElementById('show').addEventListener('click', show)
 document.getElementById('bg').addEventListener('click', background)
 
 function image () {
-  document.getElementById('picture').src = "./images/m1chip.jpg"
+  document.getElementById('picture').src = './images/m1chip.jpg'
 }
 
 function text () {
-  document.getElementById('text').innerHTML = "The Apple M1 chip beats out even the highest-end cpus"
+  document.getElementById('text').innerHTML = 'The Apple M1 chip beats out even the highest-end cpus'
 }
 
 function hide () {
-  document.getElementById('text').style.display = "none"
+  document.getElementById('text').style.display = 'none'
 }
 
 function show () {
-  document.getElementById('text').style.display = "block"
+  document.getElementById('text').style.display = 'block'
 }
 
 function background () {
-  document.body.style.backgroundColor = "lightgreen"
+  document.body.style.backgroundColor = 'lightgreen'
 }
